a325d45c23b0fbf79c3f7ed309905f81744d2afa
smartmeta-app <isdawatiirwansyah@gmail.com>
2026-07-20 09:30:28 +0700

    Update default properties file name to .env

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
 // to match the convention used in Web projects.
 secrets {
   propertiesFileName = ".env"
-  defaultPropertiesFileName = ".env.example"
+  defaultPropertiesFileName = ".env"
 }
 
 googleServices { missingGoogleServicesStrategy = MissingGoogleServicesStrategy.WARN }
